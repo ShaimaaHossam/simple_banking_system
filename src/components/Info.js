e43c6f398fb813fa61e9b36b6d28e8a279ad31a6
hagarts-site <40139825+ShaimaaHossam@users.noreply.github.com>
2022-03-19 11:01:05 +0200

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -15,8 +15,21 @@ const Info = ()=>{
     return(
         <>
             <h1 className="mt-20 mb-12 text-3xl font-bold text-center text-gray-600">Customer's info</h1>
-            <div className="mx-auto text-center">
-                <p className="text-lg text-gray-700">{customer.name}</p>
+            <div className="w-1/2 px-12 py-12 mx-auto border-2 text-start">
+                <p className="text-xl font-bold text-gray-700">Name:</p>
+                <p className="mb-6 text-lg text-gray-700">{customer.name}</p>
+
+                <p className="text-xl font-bold text-gray-700">Address:</p>
+                <p className="mb-6 text-lg text-gray-700">{customer.address}</p>
+
+                <p className="text-xl font-bold text-gray-700">Email:</p>
+                <p className="mb-6 text-lg text-gray-700">{customer.email}</p>
+
+                <p className="text-xl font-bold text-gray-700">Phone Number:</p>
+                <p className="mb-6 text-lg text-gray-700">{customer.phone}</p>
+
+                <p className="text-xl font-bold text-gray-700">Balance:</p>
+                <p className="mb-6 text-lg text-gray-700">${customer.balance}</p>
             </div>
         </>
     );
